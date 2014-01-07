e783fa097ea3dbad9e8ceed5efb3cbf87247be06
Breno Calazans <breno@vtex.com.br>
2014-01-07 19:15:32 -0200

    Atualiza e insere admin-i18n.js em outro diretorio

---
+++ dist/front-i18n/admin-i18n.js
@@ -0,0 +1,14 @@
+(function(){
+	window.vtex = window.vtex || {};
+	window.vtex.locale = "pt-BR";
+	var __esCountries = [ "argentinewine", "atria", "ciinco", "comandato", "complot", "consentidos", "decogallery", "elrosado", "falabella", "farmashop", "fravega", "gorsh", "jumbo", "jumbocolombia", "kalua", "koaladeco", "lapolar", "latiendadelreloj", "lombardi", "mascotanube", "mcgregor", "motociclo", "motociclosports", "mrpep", "mundotkm", "nutriciaencasa", "olimpica", "owonet", "perrosygatosonline", "personal", "personalempresas", "sportline", "tcexpress", "tiendauat.personal.com.br", "toscanaonline", "totto", "urgcolombia", "ventasprivadas", "verocajoyas", "viauno", "wong", "xlshop"];
+	var __ref = window.location.host;
+
+	for ( var __i = 0; __i < __esCountries.length; __i++ ) {
+		if (__ref.indexOf(__esCountries[__i]) !== -1) {
+			window.vtex.locale = "es-AR";
+			window.vtex.localePrimary = "es";
+			break;
+		}
+	}
+})();
