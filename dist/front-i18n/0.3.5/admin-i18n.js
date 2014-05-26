5572f48feea2ab1c2625413845a33bc41ce92ca7
afonso.praca <afonsoinfo@gmail.com>
2014-05-26 12:46:26 -0300

    adiciona "exito" na lista de es-AR

--- dist/front-i18n/0.3.5/admin-i18n.js
+++ dist/front-i18n/0.3.5/admin-i18n.js
@@ -1,7 +1,7 @@
 (function(){
 	window.vtex = window.vtex || {};
 	window.vtex.locale = "pt-BR";
-	var __esCountries = [ "argentinewine", "atria", "ciinco", "comandato", "complot", "consentidos", "decogallery", "elrosado", "falabella", "farmashop", "fravega", "gorsh", "jumbo", "jumbocolombia", "kalua", "koaladeco", "lapolar", "latiendadelreloj", "lombardi", "mascotanube", "mcgregor", "motociclo", "motociclosports", "mrpep", "mundotkm", "nutriciaencasa", "olimpica", "owonet", "perrosygatosonline", "personal", "personalempresas", "sportline", "tcexpress", "tiendauat.personal.com.br", "toscanaonline", "totto", "urgcolombia", "ventasprivadas", "verocajoyas", "viauno", "wong", "xlshop", "tiendasoho"];
+	var __esCountries = [ "argentinewine", "atria", "ciinco", "comandato", "complot", "consentidos", "decogallery", "elrosado", "falabella", "farmashop", "fravega", "gorsh", "jumbo", "jumbocolombia", "kalua", "koaladeco", "lapolar", "latiendadelreloj", "lombardi", "mascotanube", "mcgregor", "motociclo", "motociclosports", "mrpep", "mundotkm", "nutriciaencasa", "olimpica", "owonet", "perrosygatosonline", "personal", "personalempresas", "sportline", "tcexpress", "tiendauat.personal.com.br", "toscanaonline", "totto", "urgcolombia", "ventasprivadas", "verocajoyas", "viauno", "wong", "xlshop", "tiendasoho", exito];
 	var __ref = window.location.host;
 
 	for ( var __i = 0; __i < __esCountries.length; __i++ ) {
