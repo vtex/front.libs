045c966f2ddd05fd819e55515baad949697bfc19
Guilherme Rodrigues <gadr90@gmail.com>
2014-09-16 16:09:19 -0300

    Add curl 0.8.10-vtex.1 from vtex/curl

---
+++ dist/curl/0.8.10-vtex.1/curl/plugin/link.js
@@ -0,0 +1,2 @@
+(function(){(function(d){var b=d.document,f=/^\/\//,e;b&&(e=b.head||(b.head=b.getElementsByTagName("head")[0]));d.vtex.define("curl/plugin/link",{load:function(a,d,g,c){a=d.toUrl(a);a=a.lastIndexOf(".")<=a.lastIndexOf("/")?a+".css":a;c=a=(c="fixSchemalessUrls"in c?c.fixSchemalessUrls:b.location.protocol)?a.replace(f,c+"//"):a;a=b.createElement("link");a.rel="stylesheet";a.type="text/css";a.href=c;e.appendChild(a);g(a.sheet||a.styleSheet)}})})(this);
+}).call(this);
